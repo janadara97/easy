2b4b55f10d81201a907f9b21bedc54b4dc597d0c
janadara97 <chathurajanadara97@gmail.com>
2023-07-05 18:33:14 +0530

    circuit-breaker-psrtial-impl

--- easy-order/src/main/java/com/easy/easyorder/controller/OrderController.java
+++ easy-order/src/main/java/com/easy/easyorder/controller/OrderController.java
@@ -2,7 +2,9 @@ package com.easy.easyorder.controller;
 
 import com.easy.easyorder.model.dto.OrderRequest;
 import com.easy.easyorder.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +18,12 @@ public class OrderController {
     @Autowired
     OrderService orderService;
     @PostMapping("/create")
+    @CircuitBreaker(name = "inventory" , fallbackMethod = "fallBackMethod")
     public ResponseEntity<Object> createOrder(@RequestBody OrderRequest orderRequest){
         return orderService.createOrder(orderRequest);
+    }
+
+    public ResponseEntity<Object> fallBackMethod(OrderRequest orderRequest,RuntimeException runtimeException) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed");
     }
 }
